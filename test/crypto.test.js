f9ea122fffcd96bb9787d90c828ed8e3a7841205
Ardis Lu <ardisglu@gmail.com>
2026-09-04 22:09:33 -0700

    Properly `await` `rejects` assertions

--- test/crypto.test.js
+++ test/crypto.test.js
@@ -15,39 +15,39 @@ suite('encryptRaw and decryptRaw', { concurrency: true }, () => {
 });
 
 suite('encryptRaw', { concurrency: true }, () => {
-  test('throws on invalid version', { concurrency: true }, () => {
+  test('throws on invalid version', { concurrency: true }, async () => {
     // @ts-expect-error
-    rejects(encryptRaw({ version: 'INVALID', prf, value }), { name: 'TypeError', message: 'envpass version "INVALID" is not supported.' });
+    await rejects(encryptRaw({ version: 'INVALID', prf, value }), { name: 'TypeError', message: 'envpass version "INVALID" is not supported.' });
   });
-  test('throws on invalid prf', { concurrency: true }, () => {
+  test('throws on invalid prf', { concurrency: true }, async () => {
     // @ts-expect-error
-    rejects(encryptRaw({ version, prf: 'INVALID', value }), { name: 'TypeError', message: 'prf "INVALID" is not an ArrayBuffer or Uint8Array.' });
+    await rejects(encryptRaw({ version, prf: 'INVALID', value }), { name: 'TypeError', message: 'prf "INVALID" is not an ArrayBuffer or Uint8Array.' });
   });
-  test('throws on invalid value', { concurrency: true }, () => {
+  test('throws on invalid value', { concurrency: true }, async () => {
     // @ts-expect-error
-    rejects(encryptRaw({ version, prf, value: {} }), { name: 'TypeError', message: 'Value "{}" is not a string.' });
+    await rejects(encryptRaw({ version, prf, value: {} }), { name: 'TypeError', message: 'Value "{}" is not a string.' });
   });
 });
 
 suite('decryptRaw', { concurrency: true }, () => {
   const value = 'envpass:v1:AAA';
-  test('throws on invalid prf', { concurrency: true }, () => {
+  test('throws on invalid prf', { concurrency: true }, async () => {
     // @ts-expect-error
-    rejects(decryptRaw({ prf: 'INVALID', value }), { name: 'TypeError', message: 'prf "INVALID" is not an ArrayBuffer or Uint8Array.' });
+    await rejects(decryptRaw({ prf: 'INVALID', value }), { name: 'TypeError', message: 'prf "INVALID" is not an ArrayBuffer or Uint8Array.' });
   });
-  test('throws on invalid value', { concurrency: true }, () => {
+  test('throws on invalid value', { concurrency: true }, async () => {
     // @ts-expect-error
-    rejects(decryptRaw({ prf, value: {} }), { name: 'TypeError', message: 'Value "{}" is not a string.' });
+    await rejects(decryptRaw({ prf, value: {} }), { name: 'TypeError', message: 'Value "{}" is not a string.' });
   });
-  test('throws on invalid magic in value', { concurrency: true }, () => {
+  test('throws on invalid magic in value', { concurrency: true }, async () => {
     // @ts-expect-error
-    rejects(decryptRaw({ prf, value: 'INVALID:v1:AAA' }), { name: 'TypeError', message: 'Value "INVALID:v1:AAA" is not an envpass encrypted value.' });
+    await rejects(decryptRaw({ prf, value: 'INVALID:v1:AAA' }), { name: 'TypeError', message: 'Value "INVALID:v1:AAA" is not an envpass encrypted value.' });
   });
-  test('throws on invalid version in value', { concurrency: true }, () => {
+  test('throws on invalid version in value', { concurrency: true }, async () => {
     // @ts-expect-error
-    rejects(decryptRaw({ prf, value: 'envpass:INVALID:AAA' }), { name: 'TypeError', message: 'envpass version "INVALID" is not supported.' });
+    await rejects(decryptRaw({ prf, value: 'envpass:INVALID:AAA' }), { name: 'TypeError', message: 'envpass version "INVALID" is not supported.' });
   });
-  test('throws on invalid (non-base64) string in value', { concurrency: true }, () => {
-    rejects(decryptRaw({ prf, value: 'envpass:v1:🏴‍☠️' }), { name: 'SyntaxError', message: 'Found a character that cannot be part of a valid base64 string.' });
+  test('throws on invalid (non-base64) string in value', { concurrency: true }, async () => {
+    await rejects(decryptRaw({ prf, value: 'envpass:v1:🏴‍☠️' }), { name: 'SyntaxError', message: 'Found a character that cannot be part of a valid base64 string.' });
   });
 });
