27a7dc910ff6294b8a6fbbae3a2d529a321e6a39
Ardis Lu <ardisglu@gmail.com>
2026-09-22 23:17:43 -0700

    Make tag deletion more robust for future template changes
    The new regex will match all `<script>` or `<style>` tags in the file, regardless of attributes (e.g., SRI) or if there are multiple instances of the tag in the file.

--- test/getPrf.test.js
+++ test/getPrf.test.js
@@ -57,12 +57,12 @@ function makePostRequest(url, options = {}) {
 suite('getPrf.js (server)', () => {
   test('validates <style> tag is present in HTML template', async (t) => {
     const html = await getHTMLFromFile();
-    const modified = html.replaceAll('<style>', '');
+    const modified = html.replaceAll(/<style[^>]*>[\s\S]*?<\/style>/ig, '');
     await rejects(setupServer(t, { getHTML: () => modified }), { message: 'Could not find <style> tag in getPrf.html file.' });
   });
   test('validates <script> tag is present in HTML template', async (t) => {
     const html = await getHTMLFromFile();
-    const modified = html.replaceAll('<script type="module">', '');
+    const modified = html.replaceAll(/<script[^>]*>[\s\S]*?<\/script>/ig, '');
     await rejects(setupServer(t, { getHTML: () => modified }), { message: 'Could not find <script> tag in getPrf.html file.' });
   });
   test('valid GET works', async (t) => {
